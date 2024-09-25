39ca22649407f80d3be6967c21a7d0ee7067533d
masonFG <55590947+masonFG@users.noreply.github.com>
2024-09-25 13:26:16 +0200

    Update plot_weights.R
    enlever le print() des fonctions

--- R/plot_weights.R
+++ R/plot_weights.R
@@ -30,8 +30,6 @@ plot_weights<-function(dataset, model, select.var = NULL){
     }
   }
 
-  DATA=robust_weights(model,dataset)
-  print(colnames(DATA))
   random.labels=names(ranef(model))
   cnames<-NULL
 
@@ -48,7 +46,7 @@ plot_weights<-function(dataset, model, select.var = NULL){
   IV_supp <- data.frame(model@frame[,c(col_selected2)])
   DV =  model@frame[,as.character(formula(model)[[2]])]
   cat_i = data.frame(model@frame[,random.labels])
-  print(cnames)
+  DATA=dataset[!is.na(dataset[,as.character(formula(model)[[2]])]),]
   weighting_var = data.frame(DATA[,c(cnames,"W_e")])
 
   if(is.null(select.var)){
